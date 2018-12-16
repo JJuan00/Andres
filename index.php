3a08672b383b2d8010fa086e09382e204c917612
Juan David <40820898+JJuan00@users.noreply.github.com>
2018-12-16 02:10:35 -0500

    Login

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Gestion jardin</title>
+
+    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+    <!--Import materialize.css-->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <script src="main.js"></script>
+</head>
+<body>
+
+<div class="panel">
+		<div class="state"><br><i class="fa fa-unlock-alt"></i><br><h1>Log in</h1></div>
+		<div class="form">
+			<input placeholder='Correo electronico' type="email">
+			<input placeholder='Password' type="password">
+			<div class="login">Login</div>
+		</div>
+		<div class="fack"><a href="#"><i class="fa fa-question-circle"></i>Forgot password?</a></div>
+	</div>
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script  src="js/index.js"></script>
+
+
+
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+    
+</body>
+</html>
